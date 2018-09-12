773e210ac1cd10464b545fc406681edd12d6e152
ricagmz <2015081531@ust-ics.mygbiz.com>
2018-09-12 12:06:14 +0000

    Initial Commit

--- app/src/main/java/ricamae/gomez/com/gomezricalab3/LabAct3.java
+++ app/src/main/java/ricamae/gomez/com/gomezricalab3/LabAct3.java
@@ -3,12 +3,12 @@ package ricamae.gomez.com.gomezricalab3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class LabAct3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainactivity);
-
+        setContentView(R.layout.activity_lab_act3);
     }
 }
